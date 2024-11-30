db8b5dcf12fe4aaaadbbd1b873e88ab0b6db3d30
NightArticles <jacksonisaiah585@gmail.com>
2024-11-30 04:29:42 -0500

    Added Discount and Employee classes to the Classes folder.

---
+++ BookStore/BookStore/Classes/Discounts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
+
+namespace BookStore.Classes
+{
+    internal class Discounts
+    {
+        private string discounttype;
+        private char stor_id;
+        private short lowqty;
+        private short highqty;
+        private decimal discount;
+
+        // Constructor
+        public Discounts(string discounttype, char stor_id, short lowqty, short highqty, decimal discount)
+        {
+            this.discounttype = discounttype;
+            this.stor_id = stor_id;
+            this.lowqty = lowqty;
+            this.highqty = highqty;
+            this.discount = discount;
+        }
+
+        // Getters and Setters
+        public string Discounttype
+        {
+            get { return discounttype; }
+            set { discounttype = value; }
+        }
+        public char Stor_id
+        {
+            get { return stor_id; }
+            set { stor_id = value; }
+        }
+        public short Lowqty
+        {
+            get { return lowqty; }
+            set { lowqty = value; }
+        }
+        public short Highqty
+        {
+            get { return highqty; }
+            set { highqty = value; }
+        }
+        public decimal Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+    }
+}
---
+++ BookStore/BookStore/Classes/Employees.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
+
+namespace BookStore
+{
+    public class Employees
+    {
+        private string emp_id;
+        private string fname;
+        private string minit;
+        private string lname;
+        private short job_id;
+        private short job_lvl;
+        private char pub_Id;
+        private DateTime hire_date;
+
+
+        // Constructor
+    public Employees(string emp_id, string fname, string minit, string lname, short job_id, short job_lvl, char pub_Id, DateTime hire_date)
+        {
+            this.emp_id = emp_id;
+            this.fname = fname;
+            this.minit = minit;
+            this.lname = lname;
+            this.job_id = job_id;
+            this.job_lvl = job_lvl;
+            this.pub_Id = pub_Id;
+            this.hire_date = hire_date;
+        }
+
+        // Getters and Setters
+        public string Emp_id
+        {
+            get { return emp_id; }
+            set { emp_id = value; }
+        }
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+        public string Minit
+        {
+            get { return minit; }
+            set { minit = value; }
+        }
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public short Job_id
+        {
+            get { return job_id; }
+            set { job_id = value; }
+        }
+        public short Job_lvl
+        {
+            get { return job_lvl; }
+            set { job_lvl = value; }
+        }
+        public char Pub_Id
+        {
+            get { return pub_Id; }
+            set { pub_Id = value; }
+        }
+        public DateTime Hire_date
+        {
+            get { return hire_date; }
+            set { hire_date = value; }
+        }
+    }
+}
